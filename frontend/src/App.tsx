ef6f49b05661595ba9b01fc9f50fe70b2b35f262
Naoki Ishibashi <naokiii@amazon.co.jp>
2025-12-01 20:11:42 +0900

    [nova-edit] feat: add /image-edit route to App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,6 +9,7 @@ import Home from './pages/Home';
 import VirtualTryOn from './pages/VirtualTryOn';
 import ModelGeneration from './pages/ModelGeneration';
 import BackgroundReplacement from './pages/BackgroundReplacement';
+import ImageEdit from './pages/ImageEdit';
 import Settings from './pages/Settings';
 import './i18n'; // i18n初期化
 import './App.css';
@@ -25,6 +26,7 @@ function App() {
               <Route path="/virtual-try-on" element={<VirtualTryOn />} />
               <Route path="/model-generation" element={<ModelGeneration />} />
               <Route path="/background-replacement" element={<BackgroundReplacement />} />
+              <Route path="/image-edit" element={<ImageEdit />} />
               <Route path="/settings" element={<Settings />} />
             </Routes>
             <PWAInstallPrompt />
